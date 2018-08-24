bfe1ba03b604803c3851c7b6dd22a93cc94644d7
Michael Fudge <mafudge@gmail.com>
2018-08-24 16:04:43 -0400

    added first lab

--- lab-scalability/webapp/index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-var os = require('os');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end(`<h1>I'm ${os.hostname()}</h1><p>We are part of the same web application.`);
-}).listen(8080);
--- ubuntu-shell.cmd
+++
@@ -1,2 +0,0 @@
-docker-compose run ubuntu /bin/bash
-# docker run -it -u games ist346/ubuntu /bin/bash 
