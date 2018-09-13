827ae3b1cd96999a78570f28d076f553d761f567
Michael Fudge <mafudge@gmail.com>
2018-09-13 16:47:10 -0400

    lab G

--- lab-G/flaskapp/app.py
+++ lab-G/flaskapp/app.py
@@ -5,23 +5,23 @@ import flask
 
 app = flask.Flask(__name__)
 
-def generate_output():
+def generate_output(auto=True):
     ip = socket.gethostbyname(socket.gethostname())
     host = socket.gethostname()
     now = datetime.datetime.now()
     app.logger.debug("host:{0} ip:{1} time:{2}".format(host,ip,now))
-    output = "<h1>Sample Web Application</h1>\n" 
-    output += "<p>We are all the same application, but here are my specifics: \n" 
-    output += "<ul>\n"
-    output += "<li>HOSTNAME: <b>" + host +  "</b>\n"
-    output += "<li>IP Address: <b>" + ip +  "</b>\n"
-    output += "<li>Current Date/Time: <b>" + str(now) +  "</b>\n"
-    output += "</ul>\n"
-    return output 
+#    output = "<h1>Sample Web Application</h1>\n" 
+#    output += "<p>We are all the same application, but here are my specifics: \n" 
+#    output += "<ul>\n"
+#    output += "<li>HOSTNAME: <b>" + host +  "</b>\n"
+#    output += "<li>IP Address: <b>" + ip +  "</b>\n"
+#    output += "<li>Current Date/Time: <b>" + str(now) +  "</b>\n"
+#    output += "</ul>\n"
+    return flask.render_template('index.html', host=host, ip=ip, now=now, auto=auto) 
 
 @app.route('/')
 def index():
-    resp = flask.Response(generate_output())
+    resp = flask.Response(generate_output(auto=True))
     return resp
 
 @app.route('/slow/<int:factor>')
@@ -31,11 +31,13 @@ def slow(factor):
         for j in range(0,100000):
             x = 2-x
     #time.sleep(seconds)
-    return index()
+    resp = flask.Response(generate_output(auto=False))
+    return resp
 
 @app.route('/<key>')
 def header(key):
-    return index()
+    resp = flask.Response(generate_output(auto=False))
+    return resp
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
